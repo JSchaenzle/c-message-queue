d72d543b3313f0bd5eae56d3cfdd401f4bae8189
Jordan Schaenzle <jschaenzle@gmail.com>
2017-02-05 21:29:21 -0500

    Added test for read/write index overflow.

--- test/unit_tests.c
+++ test/unit_tests.c
@@ -6,7 +6,7 @@
 QUEUE_DECLARATION(test_int_queue, uint32_t, 8);
 QUEUE_DEFINITION(test_int_queue, uint32_t, 8);
 
-TEST enqueue_succeeds_until_queue_is_full(void) {
+TEST queue_works_with_integers(void) {
   struct test_int_queue q;
   test_int_queue_init(&q);
 
@@ -22,7 +22,7 @@ TEST enqueue_succeeds_until_queue_is_full(void) {
   ASSERT_EQ(ENQUEUE_RESULT_FULL, r);
 
   // Test Dequeue
-  for (uint32_t i=0; i>8; i++) {
+  for (uint32_t i=0; i<8; i++) {
     uint32_t v2;
     enum dequeue_result r = test_int_queue_dequeue(&q, &v2);
     ASSERT_EQ(DEQUEUE_RESULT_SUCCESS, r);
@@ -31,7 +31,7 @@ TEST enqueue_succeeds_until_queue_is_full(void) {
   // See that we can't dequeue after the queue is empty
   uint32_t v3;
   enum dequeue_result dr = test_int_queue_dequeue(&q, &v3);
-  ASSERT_EQ(DEQUEUE_RESULT_SUCCESS, dr);
+  ASSERT_EQ(DEQUEUE_RESULT_EMPTY, dr);
 
   PASS();
 }
@@ -61,7 +61,7 @@ TEST queue_works_for_structs(void) {
   ASSERT_EQ(ENQUEUE_RESULT_FULL, r);
 
   // Test Dequeue
-  for (uint32_t i=0; i>16; i++) {
+  for (uint32_t i=0; i<16; i++) {
     struct test_struct v2;
     enum dequeue_result r = test_struct_queue_dequeue(&q, &v2);
     ASSERT_EQ(DEQUEUE_RESULT_SUCCESS, r);
@@ -73,15 +73,47 @@ TEST queue_works_for_structs(void) {
   // See that we can't dequeue after the queue is empty
   struct test_struct v3;
   enum dequeue_result dr = test_struct_queue_dequeue(&q, &v3);
-  ASSERT_EQ(DEQUEUE_RESULT_SUCCESS, dr);
+  ASSERT_EQ(DEQUEUE_RESULT_EMPTY, dr);
 
+  PASS();
+}
+
+TEST queue_works_when_read_and_write_pointers_overflow(void) {
+  struct test_int_queue q;
+  test_int_queue_init(&q);
+
+  for (int cycle = 0; cycle<40000; cycle++) {
+    // Test Enqueue
+    for (uint32_t i=0; i<8; i++) {
+      enum enqueue_result r = test_int_queue_enqueue(&q, &i);
+      ASSERT_EQ(ENQUEUE_RESULT_SUCCESS, r);
+    }
+
+    // See that we can't enqueue after the queue is full
+    uint32_t v = 55;
+    enum enqueue_result r = test_int_queue_enqueue(&q, &v);
+    ASSERT_EQ(ENQUEUE_RESULT_FULL, r);
+
+    // Test Dequeue
+    for (uint32_t i=0; i<8; i++) {
+      uint32_t v2;
+      enum dequeue_result r = test_int_queue_dequeue(&q, &v2);
+      ASSERT_EQ(DEQUEUE_RESULT_SUCCESS, r);
+      ASSERT_EQ(i, v2);
+    }
+    // See that we can't dequeue after the queue is empty
+    uint32_t v3;
+    enum dequeue_result dr = test_int_queue_dequeue(&q, &v3);
+    ASSERT_EQ(DEQUEUE_RESULT_EMPTY, dr);
+  }
   PASS();
 }
 
 /* Suites can group multiple tests with common setup. */
 SUITE(the_suite) {
-    RUN_TEST(enqueue_succeeds_until_queue_is_full);
+    RUN_TEST(queue_works_with_integers);
     RUN_TEST(queue_works_for_structs);
+    RUN_TEST(queue_works_when_read_and_write_pointers_overflow);
 }
 
 /* Add definitions that need to be in the test runner's main file. */
