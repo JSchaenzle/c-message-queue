17c425177e4e98d3cf3c51475c619d79bfbeff90
Jordan Schaenzle <jschaenzle@gmail.com>
2017-02-07 19:47:06 -0500

    Added xxx_is_empty() method

--- queue.h
+++ queue.h
@@ -1,6 +1,7 @@
 #ifndef QUEUE_H
 #define QUEUE_H
 #include <stdint.h>
+#include <stdbool.h>
 #include <string.h>
 
 enum enqueue_result {
@@ -34,6 +35,7 @@ void NAME ## _init(struct NAME * p_queue)                                       
     .items = {}                                                                         \
   };                                                                                    \
 }                                                                                       \
+                                                                                        \
 enum enqueue_result NAME ##_enqueue(struct NAME  * p_queue, ITEM_TYPE * p_new_item) {   \
   uint16_t elements_in = p_queue->write_idx - p_queue->read_idx;                        \
                                                                                         \
@@ -59,6 +61,11 @@ enum dequeue_result NAME ##_dequeue(struct NAME * p_queue, ITEM_TYPE * p_item_ou
   *p_item_out = p_queue->items[i];                                                      \
                                                                                         \
   return DEQUEUE_RESULT_SUCCESS;                                                        \
-}  
+}                                                                                       \
+                                                                                        \
+bool NAME ## _is_empty(struct NAME * p_queue) {                                         \
+  return ((p_queue->write_idx - p_queue->read_idx) == 0);                               \
+}
+
 
 #endif
--- test/unit_tests.c
+++ test/unit_tests.c
@@ -10,10 +10,13 @@ TEST queue_works_with_integers(void) {
   struct test_int_queue q;
   test_int_queue_init(&q);
 
+  ASSERT_EQ(true, test_int_queue_is_empty(&q));
+
   // Test Enqueue
   for (uint32_t i=0; i<8; i++) {
     enum enqueue_result r = test_int_queue_enqueue(&q, &i);
     ASSERT_EQ(ENQUEUE_RESULT_SUCCESS, r);
+    ASSERT_EQ(false, test_int_queue_is_empty(&q));
   }
 
   // See that we can't enqueue after the queue is full
@@ -24,6 +27,7 @@ TEST queue_works_with_integers(void) {
   // Test Dequeue
   for (uint32_t i=0; i<8; i++) {
     uint32_t v2;
+    ASSERT_EQ(false, test_int_queue_is_empty(&q));
     enum dequeue_result r = test_int_queue_dequeue(&q, &v2);
     ASSERT_EQ(DEQUEUE_RESULT_SUCCESS, r);
     ASSERT_EQ(i, v2);
@@ -32,6 +36,7 @@ TEST queue_works_with_integers(void) {
   uint32_t v3;
   enum dequeue_result dr = test_int_queue_dequeue(&q, &v3);
   ASSERT_EQ(DEQUEUE_RESULT_EMPTY, dr);
+  ASSERT_EQ(true, test_int_queue_is_empty(&q));
 
   PASS();
 }
@@ -46,6 +51,7 @@ TEST queue_works_for_structs(void) {
   struct test_struct_queue q;
   test_struct_queue_init(&q);
 
+  ASSERT_EQ(true, test_struct_queue_is_empty(&q));
   // Test Enqueue
   for (uint32_t i=0; i<16; i++) {
     struct test_struct v = {
@@ -53,6 +59,7 @@ TEST queue_works_for_structs(void) {
     };
     enum enqueue_result r = test_struct_queue_enqueue(&q, &v);
     ASSERT_EQ(ENQUEUE_RESULT_SUCCESS, r);
+    ASSERT_EQ(false, test_struct_queue_is_empty(&q));
   }
 
   // See that we can't enqueue after the queue is full
@@ -63,17 +70,18 @@ TEST queue_works_for_structs(void) {
   // Test Dequeue
   for (uint32_t i=0; i<16; i++) {
     struct test_struct v2;
+    ASSERT_EQ(false, test_struct_queue_is_empty(&q));
     enum dequeue_result r = test_struct_queue_dequeue(&q, &v2);
     ASSERT_EQ(DEQUEUE_RESULT_SUCCESS, r);
     for (int j=0; j<13; j++) {
       ASSERT_EQ(i, v2.foo[j]);
-
     }
   }
   // See that we can't dequeue after the queue is empty
   struct test_struct v3;
   enum dequeue_result dr = test_struct_queue_dequeue(&q, &v3);
   ASSERT_EQ(DEQUEUE_RESULT_EMPTY, dr);
+  ASSERT_EQ(true, test_struct_queue_is_empty(&q));
 
   PASS();
 }
@@ -82,11 +90,13 @@ TEST queue_works_when_read_and_write_pointers_overflow(void) {
   struct test_int_queue q;
   test_int_queue_init(&q);
 
+  ASSERT_EQ(true, test_int_queue_is_empty(&q));
   for (int cycle = 0; cycle<40000; cycle++) {
     // Test Enqueue
     for (uint32_t i=0; i<8; i++) {
       enum enqueue_result r = test_int_queue_enqueue(&q, &i);
       ASSERT_EQ(ENQUEUE_RESULT_SUCCESS, r);
+      ASSERT_EQ(false, test_int_queue_is_empty(&q));
     }
 
     // See that we can't enqueue after the queue is full
@@ -97,6 +107,7 @@ TEST queue_works_when_read_and_write_pointers_overflow(void) {
     // Test Dequeue
     for (uint32_t i=0; i<8; i++) {
       uint32_t v2;
+      ASSERT_EQ(false, test_int_queue_is_empty(&q));
       enum dequeue_result r = test_int_queue_dequeue(&q, &v2);
       ASSERT_EQ(DEQUEUE_RESULT_SUCCESS, r);
       ASSERT_EQ(i, v2);
@@ -105,6 +116,7 @@ TEST queue_works_when_read_and_write_pointers_overflow(void) {
     uint32_t v3;
     enum dequeue_result dr = test_int_queue_dequeue(&q, &v3);
     ASSERT_EQ(DEQUEUE_RESULT_EMPTY, dr);
+    ASSERT_EQ(true, test_int_queue_is_empty(&q));
   }
   PASS();
 }
